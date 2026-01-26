619cbc01a5868f7718d79b1606eee270b53bac31
TrevorOlso <tdolson444@gmail.com>
2026-01-26 15:36:05 -0700

    Project 1 starter code

---
+++ # CS 440 – Project 1.py
@@ -0,0 +1,38 @@
+# CS 440 – Project 1
+# PCB Simulator
+# Trevor Olson 801926686
+#Tobias DiRito
+
+from enum import Enum
+from collections import deque
+
+class State(Enum):
+    NEW = 0
+    READY = 1
+    RUNNING = 2
+    WAITING = 3
+    TERMINATED = 4
+
+class PCB:
+    def __init__(self, pid, name, priority):
+        self.pid = pid
+        self.name = name
+        self.priority = priority
+        self.state = State.NEW
+        self.pc = 0
+        self.cpuTime = 0
+
+# TODO: process table (dict)
+# TODO: READY queue (deque)
+# TODO: WAITING queue (deque)
+# TODO: RUNNING reference
+# TODO: BearID auto-STATUS interval
+
+def main():
+    # TODO: parse args
+    # TODO: read trace file
+    # TODO: dispatch commands
+    pass
+
+if __name__ == "__main__":
+    main()
